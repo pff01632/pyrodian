5dfe4472ef78854caf2ca7eadded15dc251acb89
pff01632 <pff01632@gmail.com>
2021-02-02 13:38:34 +0900

    Base window implement.

--- pyrodian.py
+++ pyrodian.py
@@ -1,5 +1,12 @@
+import tkinter as tk
+
 def main():
-    print("Hello world.")
+    root = tk.Tk()
+    root.title("Parody maker by python3: Pyrodian. Ver.1.00")
+    root.resizable(False, False)
+    root.geometry("480x640")
+    root.mainloop()
+
 
 if __name__ == "__main__":
     # テスト
