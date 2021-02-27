5ecffaaf314848fa9f002a0bc801971be7e6f29b
pff01632 <pff01632@gmail.com>
2021-02-27 11:29:11 +0900

    little modified.

--- pyrodian.py
+++ pyrodian.py
@@ -1,13 +1,15 @@
 import tkinter as tk
 
+def create_frame():
+    frame = tk.Tk()
+    frame.title("Parodian by python3: Pyrodian V1.00")
+    frame.geometry("480x640")
+    frame.resizable(False, False)
+    return  frame
+
 def main():
-    root = tk.Tk()
-    root.title("Parody maker by python3: Pyrodian. Ver.1.00")
-    root.resizable(False, False)
-    root.geometry("480x640")
+    root = create_frame()
     root.mainloop()
 
-
 if __name__ == "__main__":
-    # テスト
     main()
